3152f6d6ee07a443bce5c14f8b827264ea2bd827
Henrique Pereira <hpereira0408@gmail.com>
2022-06-27 13:43:54 -0500

    changed welcome text

--- src/Loading.js
+++ src/Loading.js
@@ -2,9 +2,6 @@ import React from "react";
 
 const WelcomePage = () => (
   <h4>
-    Thank you for checking out this food app. Simply type something yummy and
-    hit search! <br />
-    <br />
     <small>
       <strong>Developed By:</strong> Henrique Pereira
     </small>
